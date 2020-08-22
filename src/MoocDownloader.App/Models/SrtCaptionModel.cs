ec316dcaf04787dd6bdca904fb6427c1df96b307
Xiaofeng Tan <xiaofeng_tan@live.com>
2020-08-22 19:50:21 +0800

    feat: add models

---
+++ src/MoocDownloader.App/Models/SrtCaptionModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace MoocDownloader.App.Models
+{
+    /// <summary>
+    /// SRT Caption model.
+    /// </summary>
+    public class SrtCaptionModel
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("languageCode")]
+        public string LanguageCode { get; set; }
+
+        [JsonProperty("lang")]
+        public string Lang { get; set; }
+
+        [JsonProperty("nosKey")]
+        public string NosKey { get; set; }
+    }
+}
