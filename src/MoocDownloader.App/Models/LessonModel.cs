6b0dd8817c1ba34e3bb53b932db846d93cb28c3f
Xiaofeng Tan <xiaofeng_tan@live.com>
2020-08-22 17:23:33 +0800

    feat: add JSON model & add docs

---
+++ src/MoocDownloader.App/Models/LessonModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace MoocDownloader.App.Models
+{
+    /// <summary>
+    /// Lesson model.
+    /// </summary>
+    public class LessonModel
+    {
+        [JsonProperty("chapterId")]
+        public long? ChapterId { get; set; }
+
+        [JsonProperty("contentId")]
+        public long? ContentId { get; set; }
+
+        [JsonProperty("contentType")]
+        public int? ContentType { get; set; }
+
+        [JsonProperty("gmtCreate")]
+        public long? GmtCreate { get; set; }
+
+        [JsonProperty("gmtModified")]
+        public long? GmtModified { get; set; }
+
+        [JsonProperty("id")]
+        public long? Id { get; set; }
+
+        [JsonProperty("isTestChecked")]
+        public bool? IsTestChecked { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("position")]
+        public int? Position { get; set; }
+
+        [JsonProperty("releaseTime")]
+        public long? ReleaseTime { get; set; }
+
+        [JsonProperty("termId")]
+        public long? TermId { get; set; }
+
+        [JsonProperty("units")]
+        public List<UnitModel> Units { get; set; }
+    }
+}
