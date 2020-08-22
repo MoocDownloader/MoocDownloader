4db8bdb90cb9b440e6165368e3af6eec19aedf6f
Xiaofeng Tan <xiaofeng_tan@live.com>
2020-08-22 17:23:33 +0800

    feat: add JSON model & add docs

---
+++ src/MoocDownloader.App/Utilities/M3U8Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoocDownloader.App.Utilities
+{
+    /// <summary>
+    /// m3u8 format file hepler class.
+    /// </summary>
+    public class M3U8Helper
+    {
+        /// <summary>
+        /// Parse M3U8 file format.
+        /// </summary>
+        /// <param name="m3u8">M3U8 file string.</param>
+        /// <returns>List of links to videos.</returns>
+        public static List<string> Parse(string m3u8)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
