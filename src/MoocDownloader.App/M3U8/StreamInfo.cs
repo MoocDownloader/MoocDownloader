603f2a14ccf69c51f5b56df317f3b5ee02e111e8
Xiaofeng Tan <xiaofeng_tan@live.com>
2020-08-30 04:52:40 +0800

    feat: refactor m3u8 parser & add ffmpeg & rename controls

---
+++ src/MoocDownloader.App/M3U8/StreamInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace MoocDownloader.App.M3U8
+{
+    public class StreamInfo
+    {
+        public string Path             { get; internal set; }
+        public long   Bandwidth        { get; internal set; }
+        public string Name             { get; internal set; }
+        public string Codecs           { get; internal set; }
+        public int    ResolutionWidth  { get; internal set; }
+        public int    ResolutionHeight { get; internal set; }
+    }
+}
