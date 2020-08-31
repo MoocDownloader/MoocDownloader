8c48c33ae6e34356e4d55b6e6077ddf6c41692f6
Xiaofeng Tan <xiaofeng_tan@live.com>
2020-09-01 03:45:31 +0800

    feat: concat ts files

---
+++ src/MoocDownloader.App/Mooc/FFmpegWorker.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using MoocDownloader.App.Models;
+
+namespace MoocDownloader.App.Mooc
+{
+    /// <summary>
+    /// FFMPEG worker.
+    /// </summary>
+    public class FFmpegWorker
+    {
+        private readonly ConcurrentQueue<CourseVideoInfo> _videoQueue;
+        private readonly EventWaitHandle                  _handle;
+
+        private bool _running = false;
+
+        private static readonly Lazy<FFmpegWorker> _lazy = new Lazy<FFmpegWorker>(() => new FFmpegWorker());
+
+
+        private FFmpegWorker()
+        {
+            _videoQueue = new ConcurrentQueue<CourseVideoInfo>();
+            _handle     = new AutoResetEvent(false);
+        }
+
+        public void Enqueue(CourseVideoInfo courseVideoInfo)
+        {
+            _videoQueue.Enqueue(courseVideoInfo);
+            _handle.Set();
+        }
+
+        private void Work()
+        {
+            _running = true;
+
+            while (_running)
+            {
+                if (_videoQueue.IsEmpty)
+                {
+                    _handle.WaitOne();
+                }
+
+                if (_videoQueue.TryDequeue(out var courseVideoInfo))
+                {
+                    Task.Factory.StartNew(() =>
+                    {
+                        try
+                        {
+
+                        }
+                        catch
+                        {
+                        }
+                    }, TaskCreationOptions.LongRunning);
+                }
+            }
+        }
+
+        /// <summary>
+        /// stop download course video.
+        /// </summary>
+        public void Stop()
+        {
+            _running = false;
+            _handle.Set();
+        }
+
+
+        /// <summary>
+        /// start download course video by ffmpeg.
+        /// </summary>
+        public void Start()
+        {
+            Work();
+        }
+    }
+}
