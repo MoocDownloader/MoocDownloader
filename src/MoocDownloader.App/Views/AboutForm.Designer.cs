70bba0a41c408ad9917763a07b9f3fafce736cbf
Xiaofeng Tan <xiaofeng_tan@live.com>
2020-09-06 23:50:38 +0800

    feat: remove 'any cpu' platform && visit link

--- src/MoocDownloader.App/Views/AboutForm.Designer.cs
+++ src/MoocDownloader.App/Views/AboutForm.Designer.cs
@@ -32,9 +32,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.label6 = new System.Windows.Forms.Label();
             this.linkLabel3 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
@@ -42,10 +40,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.Location = new System.Drawing.Point(34, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 14);
+            this.label1.Size = new System.Drawing.Size(188, 26);
             this.label1.TabIndex = 3;
             this.label1.Text = "Mooc Downloader";
             // 
@@ -53,9 +51,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(35, 74);
+            this.label2.Location = new System.Drawing.Point(35, 80);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(227, 12);
+            this.label2.Size = new System.Drawing.Size(224, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "一枚由 C# 实现的中国大学 MOOC 下载器.";
             // 
@@ -63,66 +61,48 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(37, 111);
+            this.label3.Location = new System.Drawing.Point(37, 120);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.Size = new System.Drawing.Size(49, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "贡献者: ";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Location = new System.Drawing.Point(49, 136);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(71, 12);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Weichao Liu";
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
             this.linkLabel1.LinkColor = System.Drawing.Color.Blue;
-            this.linkLabel1.Location = new System.Drawing.Point(137, 136);
+            this.linkLabel1.Location = new System.Drawing.Point(37, 188);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(155, 12);
+            this.linkLabel1.Size = new System.Drawing.Size(37, 13);
             this.linkLabel1.TabIndex = 7;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "https://github.com/lwc1st";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Location = new System.Drawing.Point(49, 163);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(77, 12);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "Xiaofeng Tan";
+            this.linkLabel1.Tag = "https://github.com/lwc1st";
+            this.linkLabel1.Text = "lwc1st";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.VisitLink);
             // 
             // linkLabel3
             // 
             this.linkLabel3.AutoSize = true;
             this.linkLabel3.BackColor = System.Drawing.Color.Transparent;
-            this.linkLabel3.Location = new System.Drawing.Point(135, 163);
+            this.linkLabel3.Location = new System.Drawing.Point(37, 160);
             this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.Size = new System.Drawing.Size(191, 12);
+            this.linkLabel3.Size = new System.Drawing.Size(71, 13);
             this.linkLabel3.TabIndex = 11;
             this.linkLabel3.TabStop = true;
-            this.linkLabel3.Text = "https://github.com/xixixixixiao";
+            this.linkLabel3.Tag = "https://github.com/xixixixixiao";
+            this.linkLabel3.Text = "Xiaofeng Tan";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.VisitLink);
             // 
             // AboutForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(450, 226);
+            this.ClientSize = new System.Drawing.Size(450, 245);
             this.Controls.Add(this.linkLabel3);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -142,9 +122,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.LinkLabel linkLabel3;
     }
 }
--- src/MoocDownloader.App/Views/AboutForm.cs
+++ src/MoocDownloader.App/Views/AboutForm.cs
@@ -9,9 +9,17 @@ namespace MoocDownloader.App.Views
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        /// <summary>
+        /// visit link of contributors.
+        /// </summary>
+        private void VisitLink(object sender, LinkLabelLinkClickedEventArgs e)
         {
+            if (sender is LinkLabel label && label.Tag is string link)
+            {
+                System.Diagnostics.Process.Start(link);
 
+                Close();
+            }
         }
     }
 }
