8791794d956c02b4b28e9a9b9c6d1ed7e5b0c44c
Xiaofeng Tan <xiaofeng_tan@live.com>
2020-09-09 16:41:09 +0800

    chore: set assembly version

--- src/MoocDownloader.App/Properties/AssemblyInfo.cs
+++ src/MoocDownloader.App/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MoocDownloader.App")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("中国大学 MOOC 下载器")]
+[assembly: AssemblyDescription("An icourse163.org MOOC downloader implemented by .NET. 一枚由 .NET 实现的中国大学 MOOC 下载器.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MoocDownloader.App")]
+[assembly: AssemblyProduct("Mooc Downloader")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
