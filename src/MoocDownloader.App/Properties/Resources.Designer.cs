764edd45c4473cbbc2ad2ec1d53123c1fc00f15c
Xiaofeng Tan <xiaofeng_tan@live.com>
2020-09-11 04:43:31 +0800

    chore: update version v1.3.1.0 & downgrade .NET Framework from 4.6.2 to 4.6

--- src/MoocDownloader.App/Properties/Resources.Designer.cs
+++ src/MoocDownloader.App/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace MoocDownloader.App.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace MoocDownloader.App.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MoocDownloader.App.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace MoocDownloader.App.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
