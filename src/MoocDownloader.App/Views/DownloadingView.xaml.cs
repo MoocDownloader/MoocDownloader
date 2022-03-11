8f2d423275b193307f6b2cfa053ca3bcd28015e1
Xiaofeng Tan <xiaofeng_tan@live.com>
2022-03-12 00:33:27 +0800

    feat: Add `Downloading` & `Finished` & `Trash` view; Add `Downloading` view layout

---
+++ src/MoocDownloader.App/Views/DownloadingView.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MoocDownloader.App.Views;
+
+/// <summary>
+/// Interaction logic for DownloadingView.xaml
+/// </summary>
+public partial class DownloadingView
+{
+    public DownloadingView()
+    {
+        InitializeComponent();
+    }
+}
