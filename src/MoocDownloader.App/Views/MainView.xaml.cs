99691d1ae26985205309db8f1b3db412f8ec9e8d
Xiaofeng Tan <xiaofeng_tan@live.com>
2022-03-11 21:02:09 +0800

    feat: Downloading tab of main view & switching tab & change app icon

--- src/MoocDownloader.App/Views/MainView.xaml.cs
+++ src/MoocDownloader.App/Views/MainView.xaml.cs
@@ -1,4 +1,10 @@
-﻿namespace MoocDownloader.App.Views;
+﻿using MahApps.Metro.IconPacks;
+using System;
+using System.Windows;
+using System.Windows.Input;
+using static MahApps.Metro.IconPacks.PackIconUniconsKind;
+
+namespace MoocDownloader.App.Views;
 
 /// <summary>
 /// The main view.
@@ -8,5 +14,37 @@ public partial class MainView
     public MainView()
     {
         InitializeComponent();
+    }
+
+    /// <summary>
+    /// Drag move the view.
+    /// </summary>
+    private void DragMove(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2)
+        {
+            WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+        else
+        {
+            DragMove();
+        }
+    }
+
+    /// <summary>
+    /// Change the state of the view.
+    /// </summary>
+    private void ChangeWindowState(object sender, RoutedEventArgs e)
+    {
+        if (sender is FrameworkElement { Tag: string action } && Enum.TryParse(action, out PackIconUniconsKind kind))
+        {
+            WindowState = kind switch
+            {
+                Minus    => WindowState.Minimized,
+                Square   => WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal,
+                Multiply => WindowState.Minimized,
+                _        => WindowState
+            };
+        }
     }
 }
