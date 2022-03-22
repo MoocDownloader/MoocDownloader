19248b4490ad0cae7200ac13f0f6f6da77f89e75
Xiaofeng Tan <xiaofeng_tan@live.com>
2022-03-23 04:30:53 +0800

    feat: Course model & binding

--- src/MoocDownloader.App/Controls/DownloadingListItem.xaml.cs
+++ src/MoocDownloader.App/Controls/DownloadingListItem.xaml.cs
@@ -1,10 +1,101 @@
-﻿namespace MoocDownloader.App.Controls;
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace MoocDownloader.App.Controls;
 
 /// <summary>
 /// Interaction logic for DownloadingListItem.xaml
 /// </summary>
 public partial class DownloadingListItem
 {
+    /// <summary>
+    /// Name of the current course.
+    /// </summary>
+    public string CourseName
+    {
+        get => (string) GetValue(CourseNameProperty);
+        set => SetValue(CourseNameProperty, value);
+    }
+
+    /// <summary>
+    /// Number of files downloaded.
+    /// </summary>
+    public int DownloadedFiles
+    {
+        get => (int) GetValue(DownloadedFilesProperty);
+        set => SetValue(DownloadedFilesProperty, value);
+    }
+
+    /// <summary>
+    /// The total number of files that will be downloaded.
+    /// </summary>
+    public int TotalFiles
+    {
+        get => (int) GetValue(TotalFilesProperty);
+        set => SetValue(TotalFilesProperty, value);
+    }
+
+    /// <summary>
+    /// Download status of the current course.
+    /// </summary>
+    public string Status
+    {
+        get => (string) GetValue(StatusProperty);
+        set => SetValue(StatusProperty, value);
+    }
+
+    /// <summary>
+    /// Cover image of the current course.
+    /// </summary>
+    public ImageSource CoverImage
+    {
+        get => (ImageSource) GetValue(CoverImageProperty);
+        set => SetValue(CoverImageProperty, value);
+    }
+
+    /// <summary>
+    /// The progress of the current course.
+    /// </summary>
+    public int ProgressRate
+    {
+        get => (int) GetValue(ProgressRateProperty);
+        set => SetValue(ProgressRateProperty, value);
+    }
+
+#region Dependency Properties
+
+    public static readonly DependencyProperty CourseNameProperty = DependencyProperty.Register
+    (
+        "CourseName", typeof(string), typeof(DownloadingListItem), new PropertyMetadata(string.Empty)
+    );
+
+    public static readonly DependencyProperty DownloadedFilesProperty = DependencyProperty.Register
+    (
+        "DownloadedFiles", typeof(int), typeof(DownloadingListItem), new PropertyMetadata(0)
+    );
+
+    public static readonly DependencyProperty TotalFilesProperty = DependencyProperty.Register
+    (
+        "TotalFiles", typeof(int), typeof(DownloadingListItem), new PropertyMetadata(0)
+    );
+
+    public static readonly DependencyProperty StatusProperty = DependencyProperty.Register
+    (
+        "Status", typeof(string), typeof(DownloadingListItem), new PropertyMetadata(string.Empty)
+    );
+
+    public static readonly DependencyProperty CoverImageProperty = DependencyProperty.Register
+    (
+        "CoverImage", typeof(ImageSource), typeof(DownloadingListItem)
+    );
+
+    public static readonly DependencyProperty ProgressRateProperty = DependencyProperty.Register
+    (
+        "ProgressRate", typeof(int), typeof(DownloadingListItem), new PropertyMetadata(0)
+    );
+
+#endregion
+
     public DownloadingListItem()
     {
         InitializeComponent();
--- src/MoocDownloader.App/ViewModels/MainViewModel.cs
+++ src/MoocDownloader.App/ViewModels/MainViewModel.cs
@@ -1,7 +1,24 @@
-﻿using Prism.Mvvm;
+﻿using MoocDownloader.App.Models;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace MoocDownloader.App.ViewModels;
 
+/// <summary>
+/// The view model of the main view.
+/// </summary>
 public class MainViewModel : BindableBase
 {
+    public ObservableCollection<CourseModel> Courses { get; set; } = new();
+
+    public MainViewModel()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        Courses.AddRange(Enumerable.Repeat(new CourseModel(), 20));
+    }
 }
