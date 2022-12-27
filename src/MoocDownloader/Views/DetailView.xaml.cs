f4bbbbe3f249fb4d4519a4934c5556d4d35724d0
Xiaofeng Tan <xiaofeng_tan@live.com>
2022-12-27 23:33:17 +0800

    feat: the detailed view of the course

---
+++ src/MoocDownloader/Views/DetailView.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MoocDownloader.Views;
+
+/// <summary>
+/// Interaction logic for DetailView.xaml
+/// </summary>
+public partial class DetailView
+{
+    public DetailView()
+    {
+        InitializeComponent();
+    }
+}
